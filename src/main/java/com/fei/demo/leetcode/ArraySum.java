8ba5ab8dd4dd9c24887120245031cb70964dcc09
unknown <zhongfei1217@163.com>
2019-11-10 16:57:08 +0800

    修改pom，添加两数之和习题

--- src/main/java/com/fei/demo/leetcode/ArraySum.java
+++ src/main/java/com/fei/demo/leetcode/ArraySum.java
@@ -1,11 +1,27 @@
 package com.fei.demo.leetcode;
 
+import java.util.Arrays;
+
 public class ArraySum {
     public static void main(String[] args) {
-
+        int[] a = new int[]{
+                1, 2, 5, 4, 3
+        };
+        int sum = 9;
+        int[] result = twoSum(a,sum);
+        System.out.println(Arrays.toString(result));
     }
 
-    public int[] twoSum(int[] nums, int target) {
-
+    public static int[] twoSum(int[] nums, int target) {
+        int[] pos = new int[2];
+        for (int pos1 = 0; pos1 < nums.length - 1; pos1++) {
+            for (int pos2 = pos1 + 1; pos2 < nums.length; pos2++) {
+                if (nums[pos1] + nums[pos2] == target) {
+                    pos[0] = pos1;
+                    pos[1] = pos2;
+                }
+            }
+        }
+        return pos;
     }
 }
